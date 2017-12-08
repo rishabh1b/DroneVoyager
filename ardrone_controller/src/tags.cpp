4bfde3e9e0bede1840b779819d886f2570afb3d5
mavera3 <michael.avera@gmail.com>
2017-12-08 00:01:29 -0500

    Add files via upload

--- ardrone_controller/src/tags.cpp
+++ ardrone_controller/src/tags.cpp
@@ -27,100 +27,56 @@
 
 // %Tag(FULLTEXT)%
 #include "ros/ros.h"
-#include "std_msgs/String.h"
+#include "geometry_msgs/Point.h"
 #include "ar_track_alvar_msgs/AlvarMarkers.h"
+#include "ardrone_control/PointArray.h"
+#include <vector>
 
-/**
- * This tutorial demonstrates simple receipt of messages over the ROS system.
- */
-// %Tag(CALLBACK)%
-/** This is the callback that will get called when a new message has arrived on the chatter topic. */
-void chatterCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg)
+
+class ReadAndCalc
 {
+public:
+  ReadAndCalc()
+  {
+    pub = n.advertise<ardrone_control::PointArray>("window_cent", 100);
+    sub = n.subscribe("ar_pose_marker", 100, &ReadAndCalc::chatterCallback, this);
+  }
 
-  /* ar_tack_alvar pacakge publishes to /ar_pose_marker topic with msg type ar_track_alvar_msgs/AlvarMarkers
-    std_msgs/Header header
-    uint32 seq
-    time stamp
-    string frame_id
-  uint32 id
-  uint32 confidence
-  geometry_msgs/PoseStamped pose
-    std_msgs/Header header
-      uint32 seq
-      time stamp
-      string frame_id
-    geometry_msgs/Pose pose
-      geometry_msgs/Point position
-        float64 x
-        float64 y
-        float64 z
-      geometry_msgs/Quaternion orientation
-        float64 x
-        float64 y
-        float64 z
-        float64 w
-  */
+  void chatterCallback(const ar_track_alvar_msgs::AlvarMarkers::ConstPtr& msg)
+  {
 
-  /* need an if statement to check how many markers have been detected. If zero, don't print with ROS_INFO*/
 
-  ROS_INFO("Tag1: [%f]", msg->markers[0].pose.pose.position.x);
-  ROS_INFO("Tag2: [%f]", msg->markers[1].pose.pose.position.x);
-  ROS_INFO("Tag3: [%f]", msg->markers[2].pose.pose.position.x);
-  ROS_INFO("Tag4: [%f]", msg->markers[3].pose.pose.position.x);
-  /*ROS_INFO("Number of Markers: [%lu]", sizeof(msg->markers));*/
-}
+    geometry_msgs::Point pnt;
+    ardrone_control::PointArray pnts;
+
+    for (int i=0; i<msg->markers.size(); ++i)
+      {
+        /*ROS_INFO_STREAM("detected tag: " << msg->markers[i].id << "position: "<< msg->markers[i].pose.pose.position);*/
+        pnt.x=msg->markers[i].pose.pose.position.x;
+        pnt.y=msg->markers[i].pose.pose.position.y;
+        pnt.z=msg->markers[i].pose.pose.position.z;
+        pnts.points.push_back(pnt);
+      }
+
+      pub.publish(pnts);
+  }
 // %EndTag(CALLBACK)%
+
+private:
+  ros::NodeHandle n;
+  ros::Publisher pub;
+  ros::Subscriber sub;
+};
 
 int main(int argc, char **argv)
 {
-  /**
-   * The ros::init() function needs to see argc and argv so that it can perform
-   * any ROS arguments and name remapping that were provided at the command line.
-   * For programmatic remappings you can use a different version of init() which takes
-   * remappings directly, but for most command-line programs, passing argc and argv is
-   * the easiest way to do it.  The third argument to init() is the name of the node.
-   *
-   * You must call one of the versions of ros::init() before using any other
-   * part of the ROS system.
-   */
+
   ros::init(argc, argv, "tags");
 
-  /**
-   * NodeHandle is the main access point to communications with the ROS system.
-   * The first NodeHandle constructed will fully initialize this node, and the last
-   * NodeHandle destructed will close down the node.
-   */
-  ros::NodeHandle n;
+  ReadAndCalc Window;
 
-  /**
-   * The subscribe() call is how you tell ROS that you want to receive messages
-   * on a given topic.  This invokes a call to the ROS
-   * master node, which keeps a registry of who is publishing and who
-   * is subscribing.  Messages are passed to a callback function, here
-   * called chatterCallback.  subscribe() returns a Subscriber object that you
-   * must hold on to until you want to unsubscribe.  When all copies of the Subscriber
-   * object go out of scope, this callback will automatically be unsubscribed from
-   * this topic.
-   *
-   * The second parameter to the subscribe() function is the size of the message
-   * queue.  If messages are arriving faster than they are being processed, this
-   * is the number of messages that will be buffered up before beginning to throw
-   * away the oldest ones.
-   */
-// %Tag(SUBSCRIBER)%
-  ros::Subscriber sub = n.subscribe("ar_pose_marker", 100, chatterCallback);
-// %EndTag(SUBSCRIBER)%
-
-  /**
-   * ros::spin() will enter a loop, pumping callbacks.  With this version, all
-   * callbacks will be called from within this thread (the main one).  ros::spin()
-   * will exit when Ctrl-C is pressed, or the node is shutdown by the master.
-   */
-// %Tag(SPIN)%
   ros::spin();
-// %EndTag(SPIN)%
 
   return 0;
 }
-// %EndTag(FULLTEXT)%
+
